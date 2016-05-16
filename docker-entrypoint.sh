fa9bc2b8c9647e72ab560609b78841f46218f1f1
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-05-15 21:59:10 -0500

    Rename .template to .env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,5 @@ npm install
 sed \
   -e 's|${COMPONENT_PATH}|'${COMPONENT_PATH}'|g' \
   -e 's|${COMPONENT_NAME}|'${COMPONENT_NAME}'|g' \
-  index.html.template > index.html
+  index.html.env > index.html
 exec npm start
