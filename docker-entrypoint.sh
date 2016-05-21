67e456f613e43498deb6d78766e0c8d2f557edfc
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-05-21 14:21:42 -0500

    Remove COMPONENT_NAME; ignore test-component

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,5 @@ npm link ${COMPONENT_PATH}
 npm install
 sed \
   -e 's|${COMPONENT_PATH}|'${COMPONENT_PATH}'|g' \
-  -e 's|${COMPONENT_NAME}|'${COMPONENT_NAME}'|g' \
   index.html.env > index.html
 exec npm start
